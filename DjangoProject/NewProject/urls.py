faf540f16fa6630e835e4f25153c7795e5c9c3c2
vaibhav <vaibhavchoubey762@gmail.com>
2025-06-17 19:05:01 +0530

    Added Applicatiosn with respective views

--- DjangoProject/NewProject/urls.py
+++ DjangoProject/NewProject/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from NewApp import views as NewAppViews
+from AnotherApp import views as AnotherAppViews
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('Welcome/', NewAppViews.WelcomePageView),
+    path('AnotherPage/', AnotherAppViews.AnotherAppViews)
 ]
